9ac0a7e4a115df59b033b93bbf3611f1484bfda6
Guillaume Lengrand <guillaume.lengrand@gmail.com>
2022-05-24 13:12:38 +0200

    home: add dynamique page for home

--- components/footer.js
+++ components/footer.js
@@ -1,37 +1,37 @@
-import React from 'react'
-import Link from 'next/link'
-import "../tailwind.config.js"
-
-
-
+import React from 'react';
+import Link from 'next/link';
+import '../tailwind.config.js';
 
 const Footer = () => (
-	<footer className="max-w-full mx-auto">
-		<div className=" content-center justify-center  bg-pwhite mb-4  ">
-			<img className="block ml-auto mr-auto w-20 sm:w-48 mt-9 " src="/static/img/ornament3.svg" alt="ornament3" />
-		</div>
+    <footer className="max-w-full mx-auto bg-pwhite">
+        <div className="content-center justify-center mb-4 ">
+            <img className="block w-20 ml-auto mr-auto sm:w-48 mt-9 " src="/static/img/ornament3.svg" alt="ornament3" />
+        </div>
 
-		<div className="w-150 bg-gray-100  content-center justify-center h-full items-center ">
-			<img className=" w-center w-full " src="/static/img/ornament2.svg" alt="ornament2" />
-		</div>
+        <div className="items-center content-center justify-center h-full bg-gray-100 w-150 ">
+            <img className="w-full  w-center" src="/static/img/ornament2.svg" alt="ornament2" />
+        </div>
 
-		<div className="main-footer  flex border-t-8 border-pdarkblue ">
-			<div className="w-2/3 justify-start  ">
-				<img className="sm:w-72 w-96 mt-6 ml-9 " src="/static/img/logo-cimetiere-footer.svg" alt="logo-cimetiere-footer" />
-			</div>
-			<div className="w-1/3"></div>
-			<div className="w-1/3  justify-end">
-				<a href="https://www.youtube.com/c/egliserussestegenevievedesbois">
-					<img className="w-14 sm:w-20 sm:ml-18 mt-14 mx-auto my-10" src="/static/img/iconmonstr-youtube-10.svg" alt="iconmonstr-youtube-10" />
-				</a>
-			</div>
-		</div>
-	</footer>
-)
+        <div className="flex border-t-8 main-footer border-pdarkblue ">
+            <div className="justify-start w-2/3 ">
+                <img
+                    className="mt-6 sm:w-72 w-96 ml-9 "
+                    src="/static/img/logo-cimetiere-footer.svg"
+                    alt="logo-cimetiere-footer"
+                />
+            </div>
+            <div className="w-1/3"></div>
+            <div className="justify-end w-1/3">
+                <a href="https://www.youtube.com/c/egliserussestegenevievedesbois">
+                    <img
+                        className="mx-auto my-10 w-14 sm:w-20 sm:ml-18 mt-14"
+                        src="/static/img/iconmonstr-youtube-10.svg"
+                        alt="iconmonstr-youtube-10"
+                    />
+                </a>
+            </div>
+        </div>
+    </footer>
+);
 
-
-export default Footer
-
-
-
-
+export default Footer;
--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,83 @@
-import Head from 'next/head'
-import { useRouter } from 'next/router'
+import Head from 'next/head';
+import {useRouter} from 'next/router';
 
-import Layout from '../components/layout'
-import AppHome from '../components/apphome'
+import Layout from '../components/layout';
+import AppHome from '../components/apphome';
+import {getLastPages} from '@/Model/page';
+import {getMediaLink, getServeurImageMedia} from 'utils/utils-serveur-image';
 
-export default function Index (props) {
+import {getMenu} from '@/Model/menu';
+import Header from '@/components/header/header';
+import PageContent from '@/components/page-template/commons/PageContent';
+import Footer from '@/components/footer';
 
-  const router = useRouter()
-  const { locale, locales, defaultLocale } = router
-  let title;
-  switch (locale) {
-    case 'fr' : 
-      title="La nécropole russe"
-      break
-    case 'en' : 
-      title="The Russian necropolis"
-      break
-    case 'ru' :
-      title="Русский некрополь"
-      break
-    default :
-      throw("Erreur. langue inconnue dans index.js : "+locale)
-  }
-	return (
-      <Layout>
-        <Head>
-          <title>{title}</title>
-        </Head>
-        <AppHome currentLanguage={locale} />
-      </Layout>
-    )
+// styles
+const bannerStyles = {
+    height: 360,
+};
+
+export default function Index({menu, page}) {
+    const router = useRouter();
+    const {locale, locales, defaultLocale} = router;
+    let title;
+    switch (locale) {
+        case 'fr':
+            title = 'La nécropole russe';
+            break;
+        case 'en':
+            title = 'The Russian necropolis';
+            break;
+        case 'ru':
+            title = 'Русский некрополь';
+            break;
+        default:
+            throw 'Erreur. langue inconnue dans index.js : ' + locale;
+    }
+    return (
+        <div className="bg-pwhite">
+            <Head>
+                <title>{title}</title>
+            </Head>
+
+            {page ? (
+                <>
+                    {/* Header */}
+                    {menu && <Header menu={menu.data} currentLanguage={locale} />}
+                    {page.bandeau && (
+                        <div style={bannerStyles} className="">
+                            <img
+                                className="block object-cover w-full h-full"
+                                src={getMediaLink(page.bandeau.public_path)}
+                                alt=""
+                            />
+                        </div>
+                    )}
+                    {/* Content */}
+                    <main className="h-full max-w-screen-xl px-10 py-10 mx-auto bg-pwhite">
+                        <PageContent blocks={page.blocks} pageName={page.pageName} />
+                    </main>
+                    <Footer />
+                </>
+            ) : (
+                <AppHome currentLanguage={locale} />
+            )}
+        </div>
+    );
 }
 
+export const getServerSideProps = async ctx => {
+    const menu = await getMenu(ctx.locale);
+    let homePages = await getLastPages(ctx.locale, 'home', 1);
+    // so that we can directly manipulate JS object in Components
+    if (homePages && homePages[0]) {
+        const bandeau = await getServeurImageMedia(homePages[0].bandeau_id);
+        homePages[0].bandeau = bandeau;
+        homePages[0].blocks = JSON.parse(homePages[0].blocks);
+    }
+    return {
+        props: {
+            menu,
+            page: homePages ? homePages[0] : null,
+        },
+    };
+};
