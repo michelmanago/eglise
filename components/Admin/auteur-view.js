29aca164a60c287df5d1a090e69b23c68147d90e
Guillaume Lengrand <guillaume.lengrand@gmail.com>
2022-06-09 17:01:45 +0200

    User: change view for author user

--- components/Admin/auteur-view.js
+++ components/Admin/auteur-view.js
@@ -3,19 +3,9 @@ import Link from 'next/link';
 export default function AuteurView({}) {
     return (
         <>
-            <Link href="/admin/defunts">
+            <Link href="/admin/articles">
                 <a className="h-6 px-4 py-1 mb-20 mr-6 -mt-24 text-xs text-center text-white transition duration-300 ease-in-out rounded-lg sm:text-sm bg-pgold sm:-mt-20 sm:h-7 hover:bg-pblue">
-                    Editer un défunt
-                </a>
-            </Link>
-            <Link href="/admin/pageContent">
-                <a className="h-6 px-4 py-1 mb-20 mr-6 -mt-24 text-xs text-center text-white transition duration-300 ease-in-out rounded-lg sm:text-sm bg-pgold sm:-mt-20 sm:h-7 hover:bg-pblue">
-                    Editer PageContent
-                </a>
-            </Link>
-            <Link href="/admin/pieceJointe">
-                <a className="h-6 px-4 py-1 mb-20 mr-6 -mt-24 text-xs text-center text-white transition duration-300 ease-in-out rounded-lg sm:text-sm bg-pgold sm:-mt-20 sm:h-7 hover:bg-pblue">
-                    Pieces Jointes
+                    Editer un article
                 </a>
             </Link>
         </>
