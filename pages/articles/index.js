5ecea202b78ecfef599958b9797495526ed7b3d3
Guillaume Lengrand <guillaume.lengrand@gmail.com>
2023-05-02 14:58:59 +0200

    Article: filter article in draft

--- pages/articles/index.js
+++ pages/articles/index.js
@@ -38,32 +38,34 @@ export default function Articles({menu, articles}) {
                     <div className="container mx-auto sm:flex">
                         <main className="mx-2 mt-4 sm:mx-20">
                             <div className="flex flex-wrap">
-                                {articles?.array?.map(article => (
-                                    <div className="w-full px-2 mt-2 sm:w-1/2 md:w-1/3" key={article.id}>
-                                        {console.log(article.bandeau)}
-                                        <Link href={`/${article.pageSlug}`}>
-                                            <a className="flex flex-col gap-1">
-                                                {article.bandeau ? (
-                                                    <img
-                                                        className="object-cover mx-auto"
-                                                        style={{aspectRatio: '4 / 3'}}
-                                                        src={`${apiMediaUrl}${article.bandeau.public_path}`}
-                                                    />
-                                                ) : (
-                                                    <img
-                                                        className="object-cover mx-auto border border-black"
-                                                        style={{aspectRatio: '4 / 3'}}
-                                                        src="/static/img/default-article-img.jpg"
-                                                    />
-                                                )}
-                                                <h3 className="inline-block">{article.pageName}</h3>
-                                                <div className="inline-block text-sm">
-                                                    {t('common:article_date')} {getProperDate(article.created_at)}
-                                                </div>
-                                            </a>
-                                        </Link>
-                                    </div>
-                                ))}
+                                {articles?.array
+                                    ?.filter(a => a.draft === false)
+                                    .map(article => (
+                                        <div className="w-full px-2 mt-2 sm:w-1/2 md:w-1/3" key={article.id}>
+                                            {console.log(article.bandeau)}
+                                            <Link href={`/${article.pageSlug}`}>
+                                                <a className="flex flex-col gap-1">
+                                                    {article.bandeau ? (
+                                                        <img
+                                                            className="object-cover mx-auto"
+                                                            style={{aspectRatio: '4 / 3'}}
+                                                            src={`${apiMediaUrl}${article.bandeau.public_path}`}
+                                                        />
+                                                    ) : (
+                                                        <img
+                                                            className="object-cover mx-auto border border-black"
+                                                            style={{aspectRatio: '4 / 3'}}
+                                                            src="/static/img/default-article-img.jpg"
+                                                        />
+                                                    )}
+                                                    <h3 className="inline-block">{article.pageName}</h3>
+                                                    <div className="inline-block text-sm">
+                                                        {t('common:article_date')} {getProperDate(article.created_at)}
+                                                    </div>
+                                                </a>
+                                            </Link>
+                                        </div>
+                                    ))}
                             </div>
                         </main>
                     </div>
